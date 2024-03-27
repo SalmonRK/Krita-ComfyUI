97fc51a8c65bb91157c5a48c67a675f19aa485a8
SalmonRK <130884337+SalmonRK@users.noreply.github.com>
2024-03-27 23:26:30 +0700

    Update install comfy-manager.bat

--- install comfy-manager.bat
+++ install comfy-manager.bat
@@ -1,2 +1,5 @@
+@echo off
 .\python\python.exe -s -m pip install gitpython
 .\python\python.exe -c "import git; git.Repo.clone_from('https://github.com/ltdrdata/ComfyUI-Manager', './ComfyUI/custom_nodes/ComfyUI-Manager')"
+.\python\python.exe -c "import git; git.Repo.clone_from('https://github.com/ZHO-ZHO-ZHO/ComfyUI-BRIA_AI-RMBG', './ComfyUI/custom_nodes/ComfyUI-BRIA_AI-RMBG')"
+curl -Lo .\ComfyUI\custom_nodes\ComfyUI-BRIA_AI-RMBG\RMBG-1.4\model.pth https://huggingface.co/briaai/RMBG-1.4/resolve/main/model.pth --ssl-no-revoke

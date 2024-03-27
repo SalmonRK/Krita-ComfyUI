3c48304df624db69c6c1c663d008527c26fd78e7
SalmonRK <130884337+SalmonRK@users.noreply.github.com>
2024-03-27 23:12:36 +0700

    Add files via upload

---
+++ install comfy-manager.bat
@@ -0,0 +1,2 @@
+.\python\python.exe -s -m pip install gitpython
+.\python\python.exe -c "import git; git.Repo.clone_from('https://github.com/ltdrdata/ComfyUI-Manager', './ComfyUI/custom_nodes/ComfyUI-Manager')"
